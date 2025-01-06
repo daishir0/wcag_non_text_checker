619b7427087f4624cc9001eb9ba524564e368314
daishir0 <daishiro.hirashima@gmail.com>
2025-01-06 15:34:14 +0900

    Create config.sample.py

---
+++ config.sample.py
@@ -0,0 +1,9 @@
+# Anthropic API設定
+ANTHROPIC_API_KEY = "your-api-key-here"
+
+# Chrome設定
+CHROME_BINARY_PATH = "/usr/bin/google-chrome"
+CHROME_DRIVER_PATH = "/usr/bin/chromedriver"
+
+# デバッグ設定
+DEBUG = False 
